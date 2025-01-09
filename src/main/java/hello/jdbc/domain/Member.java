18d57af93f04b4565cb96ee3e834e84f49370d1d
jinhyeon <tiemo0708@naver.com>
2025-01-10 04:38:07 +0900

    feat: JDBC개발 - 조회

--- src/main/java/hello/jdbc/domain/Member.java
+++ src/main/java/hello/jdbc/domain/Member.java
@@ -8,10 +8,11 @@ public class Member {
     private String memberId;
     private int money;
 
+    public Member() {
+    }
+
     public Member(String memberId, int money) {
         this.memberId = memberId;
         this.money = money;
     }
-
-
 }
